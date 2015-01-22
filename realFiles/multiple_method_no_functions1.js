58478d7380a9d930a4871412e363b4206d9fe39a
Shannon Ernst <shannon@shannon-VirtualBox.(none)>
2015-01-22 12:59:25 -0800

    Second batch of js

---
+++ realFiles/multiple_method_no_functions1.js
@@ -0,0 +1,8 @@
+function multiply(a,b){
+	multi = a*b;
+	return multi;
+}
+function main(){
+	a = 5;
+	b = 7;
+}
---
+++ realFiles/multiple_method_no_functions2.js
@@ -0,0 +1,9 @@
+function division(a,b){
+	divi = a/b;
+	return divi;
+}
+function main(){
+	a = 6;
+	b = 2;
+	sum = a + 4;
+}
