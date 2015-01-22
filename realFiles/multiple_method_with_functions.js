357067be65cae174fd018f717774fca39e906d0b
Shannon Ernst <shannon@shannon-VirtualBox.(none)>
2015-01-22 13:05:28 -0800

    Last batch js

---
+++ realFiles/multiple_method_with_functions.js
@@ -0,0 +1,7 @@
+function hello(){
+	hello = "Hello World!";
+	console.log(hello);
+}
+function main(){
+	console.log("Hello World!");
+}
---
+++ realFiles/multiple_method_with_functions_method_call.js
@@ -0,0 +1,16 @@
+function addition(a,b){
+	add = a+b;
+	return add;
+}
+function subtraction(a,b){
+	sub = a-b;
+	return sub;
+}
+function main(){
+	a=7;
+	b=5;
+	addition(a,b);
+	subtraction(a,b);
+	console.log(addition);
+	console.log(subtraction);
+}
