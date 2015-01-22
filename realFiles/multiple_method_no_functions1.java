d6cb33200ee1341acc8f7ea86a368a465cdcb5d3
Shannon Ernst <shannon@shannon-VirtualBox.(none)>
2015-01-22 12:41:56 -0800

    Next batch

---
+++ realFiles/multiple_method_no_functions1.java
@@ -0,0 +1,9 @@
+public static int multiply(int a, int b){
+	int multi;
+	multi = a * b;
+	return multi;
+}
+public static void main(String[] args){
+	int a = 5;
+	int b = 7;
+}
---
+++ realFiles/multiple_method_no_functions2.java
@@ -0,0 +1,11 @@
+public static int division(int a, int b){
+	int divi;
+	divi = a/b;
+	return divi;
+}
+public static void(String[] args){
+	int a = 6;
+	int b = 2;
+	int sum = a + 4;
+}
+
